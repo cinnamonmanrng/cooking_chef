e7a324be4a8f72660033895e8dff709fb74f7662
Gert Leja <92955729+cinnamonmanrng@users.noreply.github.com>
2023-02-16 23:41:29 +0000

    Base game files
    this is the work so far

---
+++ chef_cg.py
@@ -0,0 +1,105 @@
+# text colour: print("\033[1;32;40m Bright Green  \n")
+# reset colour: print("\033[0m Hiya")
+
+# common: \033[32m
+# uncommon: \033[32;1m
+# rare: \033[34m
+# epic: \033[35m
+# legendary: \033[31;1m
+# mythic/unique: \033[36;1m
+import time
+from chef_skeleton import *
+import random
+
+def timer(duration, recipe_list):
+	print(recipe_list[0].name, "Has started cooking:", duration)
+	time.sleep(duration) # might need to run this in while loop??
+	print(recipe_list[0].name, "Has finished cooking, you have gained:", recipe_list[0].exp_value, "XP!")
+
+def level_up_1(player):
+	if player.level == 0:
+		if player.experience >= player.max_xp:
+			player.level = 1
+			player.max_xp = 2500
+			player.next_level = 2
+			print("\033[33;1mYour rating has increased!!\033[0m") 
+			player.print_status()
+		else:
+			player.print_status()
+
+def level_up_2(player):
+	if player.level == 1:
+		if player.experience >= player.max_xp:
+			player.level = 2
+			player.max_xp = 5000
+			player.next_level = 3
+
+def level_up_3(player):
+	if player.level == 2:
+		if player.experience >= player.max_xp:
+			player.level = 3
+			player.max_xp = 10000
+			player.next_level = 4
+
+def level_up_4(player):
+	if player.level == 3:
+		if player.experience >= player.max_xp:
+			player.level = 4
+			player.max_xp = 20000
+			player.next_level = 5
+
+def level_up_5(player):
+	if player.level == 4:
+		if player.experience >= player.max_xp:
+			player.level = 5
+			player.max_xp = "MAX"
+			player.next_level = "MAX"
+		
+
+# game loop
+def main():
+	while True:	
+		recipe1 = Recipe("\033[32mRoasted Potatoes\033[0m", 30, 0, 5)
+		recipe2 = Recipe("\033[32mChicken with Roasted Potatoes\033[0m", 45, 0, 150)
+		recipe3 = Recipe("\033[32mMashed Potatoes\033[0m", 20, 0, 100)
+		recipe_list = (recipe1, recipe2, recipe3)
+		duration = recipe_list[0].timer # need to make duration take data from a tuple
+
+		print("Hello young chef, welcome to the kitchen!")
+		time.sleep(1)
+		playername = input("To start off, what is your name?: ")
+
+		player = Player(playername, 0, 0, 1000, 1)
+
+		print("\n")
+		player.print_status()
+		print("\n")
+		time.sleep(1)
+
+		print("To become a true master of the cooking arts, you will need to learn some recipes")
+		time.sleep(1.7)
+		print("How about I teach you one right now!")
+		time.sleep(1)
+
+		Recipe.recipe_inv.append(recipe1)
+		print("\033[33;1mNew recipe gained!!\033[0m")
+		time.sleep(1)
+		Recipe.check_recipe_inv()
+
+		def tutorial_cooking_input(recipe_list):
+			cooking_input = input(" Enter Y to start cooking: ")
+			if cooking_input.upper() == "Y":
+				timer(duration, recipe_list)
+				player.experience += recipe_list[0].exp_value
+				level_up_1(player)
+			else:
+				print("Please enter Y to continue!")
+				tutorial_cooking_input(recipe_list)
+
+		print("Let's start off by practicing your cooking skills")
+		time.sleep(1)
+		print("So first you will want to select the option to cook, which I will give you now")
+		tutorial_cooking_input(recipe_list)
+
+		break
+main()
---
+++ chef_skeleton.py
@@ -0,0 +1,131 @@
+# chef simulator
+	# add player (chef) class *
+	# add stats (1 - 5 stars) *
+	# develop stats by cooking and other stuff (gain xp from recipes) / timer ending gives xp to player *
+		# will need a lot of if statements, optimisation problems inc.
+		# also need to fix timer to recognise multiple recipes without manual input
+	# equipment and equipment upgrading (equipment parts to upgrade equipment or forge a new one)
+	# recipe preparation mechanics (time, queue, booster items affect time)
+	# create a list of recipe names, unlockable by quality (upgradeable with items) also how much time they take *
+	# late game booster items (hats, clothes and whatever)
+	# queue for cooking recipes
+	# tutorial
+	# saving and loading game mechanics
+
+# cd desktop/'gert work'/'gert coding'/'chef game'
+
+class Player:
+	player_inventory = []
+
+	def __init__(self, name, level, experience, max_xp, next_level):
+		self.name = name
+		self.level = level
+		self.experience = experience
+		self.max_xp = max_xp
+		self.next_level = next_level
+
+	def print_status(self):
+		print(f"{self.name} | Rating: {Experience.get_level_star(self)} | XP: {self.experience} / {self.max_xp} until {Experience.next_level_star(self)}")
+
+	def check_inventory(self):
+		if len(self.inventory) == 0:
+			print("Your inventory is empty, check again later!")
+		else:
+			for item in self.inventory:
+				Item.print_info()
+
+class Experience(Player):
+	def __init__(self, experience, next_level, level):
+		super().__init__(experience, next_level, level)
+		self.next_level = 0
+
+	def get_level_star(self):
+		if self.level == 0:
+			return "Unrecognised"
+		elif self.level == 1:
+			return "★"
+		elif self.level == 2:
+			return "★★"
+		elif self.level == 3:
+			return "★★★"
+		elif self.level == 4:
+			return "★★★★"
+		elif self.level == 5:
+			return "★★★★★"
+		else: 
+			return "Invalid Level"
+
+	def next_level_star(self):
+		if self.level == 0 and self.next_level == 1:
+				return "☆"
+		elif self.level == 1 and self.next_level == 2:
+			return "★☆"
+		elif self.level == 2 and self.next_level == 3:
+			return "★★☆"
+		elif self.level == 3 and self.next_level == 4:
+			return "★★★☆"
+		elif self.level == 4 and self.next_level == 5:
+			return "★★★★☆"
+		elif self.level == 5 and self.next_level == "MAX":
+			return "★★★★★"
+		else:
+			return "Invalid next level"
+
+
+class Item:
+	def __init__(self, name, description, rating, quantity):
+		self.name = name
+		self.description = description
+		self.rating = rating
+		self.quantity = quantity
+	def print_info(self):
+		print(f'{self.name}: {self.description} | Rating: {self.rating} | Amount: {self.quantity}') # print info for player inventory check
+
+class Equipment(Item):
+	def __init__(self, name, description, rating, quantity):
+		super().__init__(name, description, rating, quantity)
+
+class Equip_part(Item):
+	def __init__(self, name, description, rating, quantity):
+		super().__init__(name, description, rating, quantity)
+
+class Recipe:
+	recipe_inv = []
+
+	def __init__(self, name, exp_value, quality, timer):
+		self.name = name
+		self.exp_value = exp_value
+		self.quality = quality
+		self.timer = timer # in seconds
+		# self.quality_x = quality_x # quality multiplier (0 = 1 | 1 = 1.1 | 2 = 1.2 | 3 = 1.3 | 4 = 1.5 | 5 = 1.8)
+
+#	def gain_xp_recipe(self): # not sure why but this does not add experience points to player when called
+#		if self.timer <= 0:
+#			Player.experience += self.exp_value
+
+	@classmethod
+	def check_recipe_inv(cls):
+		if len(cls.recipe_inv) == 0:
+			print("You do not know any recipes, check again later!")
+		else:
+			for recipe in cls.recipe_inv:
+				recipe.print_info()
+
+	def recipe_quality(self):
+		if self.quality == 0:
+			return "☆"
+		elif self.quality == 1:
+			return "★"
+		elif self.quality == 2:
+			return "★★"
+		elif self.quality == 3:
+			return "★★★"
+		elif self.quality == 4:
+			return "★★★★"
+		elif self.quality == 5:
+			return "★★★★★"
+		else:
+			print("Unrecognised quality")
+
+	def print_info(self):
+		print(f'{self.name}: XP gained from this recipe: {self.exp_value} | Quality: {Recipe.recipe_quality(self)} | Time to cook: {self.timer}')
